78a88712a5ae3ecb29523752178260706d20c6f7
ParkerRoseBurck <48800361+ParkerRoseBurck@users.noreply.github.com>
2019-10-21 08:54:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+//Get ABOUT ME page
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About Me' });
+});
+//Get PROJECTS page
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects' });
+});
+//Get SERVICES page
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Services' });
+});
+//Get CONTACT page
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
